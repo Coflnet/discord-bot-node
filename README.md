2c4b1485e81768002d6e7eb821be82c7887b4d36
thomas <thesaltysweat294@gmail.com>
2021-10-29 14:45:57 -0500

    ur mom Co-authored-by: matthias-luger <matthias-luger@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Cofl bot
+
+## Environment variables
+
+### TOKEN
+The discord bot token for your server
+
+### CHANNEL_ID_BUGREPORT
+The ID of the bug-report channel
+
+### CHANNEL_ID_SUPPORT´
+The ID of the support channel
+
+### CHANNEL_ID_SUGGESTIONS
+The ID of the suggestions channel
--- bot.js
+++ bot.js
@@ -1,4 +1,3 @@
-
 const { Client, Intents, ThreadChannel, Channel } = require('discord.js');
 const dotenv = require('dotenv')
 dotenv.config()
@@ -11,67 +10,67 @@ const client = new Client({ intents: myIntents });
 
 client.on('messageCreate', (message) => {
 
-  //if (message.channelId)
-  var text = message.content.toLowerCase();
-  if (message.author.bot) {
-    return; // the bot wont respond to itself 
-  }
+    //if (message.channelId)
+    var text = message.content.toLowerCase();
+    if (message.author.bot) {
+        return; // the bot wont respond to itself 
+    }
 
-  if (text.split(" ").length == 1) {
-    // only one word
-    return message.delete()
-  } else if (text.indexOf("@everyone") >= 0) {
-    return message.delete()
-  }
+    if (text.split(" ").length == 1) {
+        // only one word
+        return message.delete()
+    } else if (text.indexOf("@everyone") >= 0) {
+        return message.delete()
+    }
 
-  if (text.indexOf("mod") >= 0 && text.indexOf("bannable") >= 0) {
-    message.channel.send("The mod is not bannable\nhere is the source code if you wish to look at it https://github.com/Coflnet/HypixelSkyblock/tree/separation#get-startedusage");
-  }
-  if (text.indexOf("mod") >= 0 && text.indexOf("download") >= 0) {
-    message.channel.send("you can download the mod in the channel labled\n <#902240719937368104>")
-  }
-  if (text.indexOf("should") >= 0 && text.indexOf("buy") >= 0 && text.indexOf("premium") >= 0) {
-    message.channel.send("no dont buy\nunless you like the benifits listed on https://sky.coflnet.com/premium");
-  }
-  if (text.indexOf("doesnt") >= 0 && text.indexOf("make") >= 0 && text.indexOf("money") >= 0) {
-    message.channel.send("it uses medium price which means it runs on statistics aka the average price of said item");
-  }
-  if (text.indexOf("mod") >= 0 && text.indexOf("free") >= 0) {
-    message.channel.send("the mod is free to use for free and paid version, you can download it in mod-releases");
-  }
-  if (text.indexOf("whats") >= 0 && text.indexOf("tfm") >= 0) {
-    message.channel.send("theres a discord link in @ThomasW profile");
-  }
+    if (text.indexOf("mod") >= 0 && text.indexOf("bannable") >= 0) {
+        message.channel.send("The mod is not bannable\nhere is the source code if you wish to look at it https://github.com/Coflnet/HypixelSkyblock/tree/separation#get-startedusage");
+    }
+    if (text.indexOf("mod") >= 0 && text.indexOf("download") >= 0) {
+        message.channel.send("you can download the mod in the channel labled\n <#902240719937368104>")
+    }
+    if (text.indexOf("should") >= 0 && text.indexOf("buy") >= 0 && text.indexOf("premium") >= 0) {
+        message.channel.send("no dont buy\nunless you like the benifits listed on https://sky.coflnet.com/premium");
+    }
+    if (text.indexOf("doesnt") >= 0 && text.indexOf("make") >= 0 && text.indexOf("money") >= 0) {
+        message.channel.send("it uses medium price which means it runs on statistics aka the average price of said item");
+    }
+    if (text.indexOf("mod") >= 0 && text.indexOf("free") >= 0) {
+        message.channel.send("the mod is free to use for free and paid version, you can download it in mod-releases");
+    }
+    if (text.indexOf("whats") >= 0 && text.indexOf("tfm") >= 0) {
+        message.channel.send("theres a discord link in @ThomasW profile");
+    }
 
-  if (message.channel.name === "support") {
-    message.channel.threads
-      .create({
-        name: 'Support Help',
-        autoArchiveDuration: 1440,
-        startMessage: message.id,
-        reason: 'Needed a separate thread for moderation',
-      })
-  }
+    if (message.channel.id === process.env.CHANNEL_ID_SUPPORT) {
+        message.channel.threads
+            .create({
+                name: 'Support Help',
+                autoArchiveDuration: 1440,
+                startMessage: message.id,
+                reason: 'Needed a separate thread for moderation',
+            })
+    }
 
-  if (message.channel.name === "🐛bug-report") {
-    channel.threads
-      .create({
-        name: 'Bug Help',
-        autoArchiveDuration: 1440,
-        startMessage: message.id,
-        reason: 'help with bug',
-      })
-  }
+    if (message.channel.id === process.env.CHANNEL_ID_BUGREPORT) {
+        message.channel.threads
+            .create({
+                name: 'Bug Help',
+                autoArchiveDuration: 1440,
+                startMessage: message.id,
+                reason: 'help with bug',
+            })
+    }
 
-  if (message.channel.name === "suggestions") {
-    channel.threads
-      .create({
-        name: 'Suggestion Idea',
-        autoArchiveDuration: 1440,
-        startMessage: message.id,
-        reason: 'To help someone with their suggestion',
-      })
-  }
+    if (message.channel.id === process.env.CHANNEL_ID_SUGGESTIONS) {
+        message.channel.threads
+            .create({
+                name: 'Suggestion Idea',
+                autoArchiveDuration: 1440,
+                startMessage: message.id,
+                reason: 'To help someone with their suggestion',
+            })
+    }
 })
 
 
