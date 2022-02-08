9c3b493042453e749729ef5e298da25626bbaaa7
thomas <thesaltysweat294@gmail.com>
2022-02-08 17:06:56 -0600

    got rid of somthing

--- bot.js
+++ bot.js
@@ -83,9 +83,6 @@ function checkForThreadCreation(message) {
     let text = message.content.toLowerCase();
     if (message.channel.id === process.env.CHANNEL_ID_SUPPORT) {
         createAnswerThread(message, 'Support Help', 'Needed a separate thread for moderation', thread => { sendAnswer(thread, text) });
-        client.users.fetch(process.env.TENTAMENS_ID, false).then ((users) => {
-            users.send((message.author.username) + " Made a support ticket " + (message.content))
-        })
         return true;
     }
 
@@ -93,18 +90,12 @@ function checkForThreadCreation(message) {
         createAnswerThread(message, 'Bug Help', 'help with bug', thread => {
             thread.send("Thank you for making a ticket\nPlease state the below\n- What you did\n- What you intended to do\n- what happened (even better if you take a screenshot/video of it\n- What you expected\nTry to be as precise and complete as possible. (Its faster to read some duplicate text than to ask you something)\nIf you use the mod please also use /cofl report (optional message) to easily create a report.)");
             sendAnswer(thread, text);
-            client.users.fetch(process.env.TENTAMENS_ID, false).then ((users) => {
-                users.send((message.author.username) + " Made a bug report saying " + (message.content))
-            })
         });
         return true;
     }
 
     if (message.channel.id === process.env.CHANNEL_ID_SUGGESTIONS) {
         createAnswerThread(message, 'Suggestion Idea', 'To help someone with their suggestion', thread => { sendAnswer(thread, text) });
-        client.users.fetch(process.env.TENTAMENS_ID, false).then ((users) => {
-            users.send((message.author.username) + " Made a suggestion " + (message.content))
-        })
         return true;
         
     }
