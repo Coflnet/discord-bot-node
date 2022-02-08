0d5cb143e747e8a2559df51b5f60206d381d27a8
thomas <thesaltysweat294@gmail.com>
2022-02-08 16:59:00 -0600

    changed profit command to use embeds

--- commands/profitCommands.js
+++ commands/profitCommands.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const fetch = require('cross-fetch')
+const fetch = require('cross-fetch');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,26 +8,57 @@ module.exports = {
         .setDescription('how much profit some has made in the last 7 days')
         .addStringOption(option =>
             option.setName('name')
-            .setDescription('the username of the player')
-            .setRequired(true)
+                .setDescription('the username of the player')
+                .setRequired(true)
         ),
     async execute(interaction) {
         let name = interaction.options.getString('name');
         if (name.split(" ").length > 1) {
-            await interaction.reply("Please enter a valid username")
+            await interaction.reply({ embeds: [playerNotFoundEmbed()] })
             return
         }
         let res = await fetch(`https://sky.coflnet.com/api/search/player/${name}`);
         let playerResponse = await res.json();
         if (playerResponse.Slug === "player_not_found") {
-            await interaction.reply("The name you provided was not found please check your spelling");
+            await interaction.reply({ embeds: [playerNotFoundEmbed()] })
         } else {
-            await interaction.reply("fetching data...")
+            await interaction.reply({ embeds: [fetchingData(interaction)] })
             let response = await fetch(`https://sky.coflnet.com/api/flip/stats/player/${playerResponse[0].uuid}`);
-            let data = await response.json();
-            await interaction.editReply((name) + ' has made ' + formatToPriceToShorten(data.totalProfit, 0) + " In the last 7 days")
+            let playerData = await response.json();
+            await interaction.editReply({ embeds: [profitEmbed(interaction, playerResponse, playerData, name)] })
         }
+
     }
+
+}
+
+function playerNotFoundEmbed() {
+    const embeded = new MessageEmbed()
+    .setColor('#0099ff')
+    .setAuthor('Error!')
+    .setDescription('The Name you entered was not found please check your spelling')
+    return embeded
+}
+
+function profitEmbed(interaction, playerResponse, playerData, name) {
+    const playerID = (interaction.member.user.id)
+    const exampleEmbed = new MessageEmbed()
+        .setColor('#0099ff')
+        .setURL('https://discord.js.org/')
+        .setAuthor('Flipping Profit')
+        .setDescription("<@" + playerID + ">")
+        .setThumbnail(String(`https://crafatar.com/renders/head/${playerResponse[0].uuid}`))
+        .addField((String(name)) + " has made", (String(formatToPriceToShorten(playerData.totalProfit, 0) + " in the last 7 days")), true)
+        .setTimestamp()
+    return exampleEmbed
+}
+
+function fetchingData(interaction) {
+    const fetchingData = new MessageEmbed()
+        .setColor('#0099ff')
+        .setAuthor((String(interaction.member.user.tag)))
+        .setDescription('Fetching data...')
+    return fetchingData
 }
 
 function formatToPriceToShorten(num, decimals) {
