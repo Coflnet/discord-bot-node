e39d868b2c34801d0d8fd67ffbd25e87e30c3110
Matthias Luger <malua007@live.de>
2022-05-16 17:44:46 +0200

    improved and simplified sellprofit command

--- commands/profitAfterSellCommand.js
+++ commands/profitAfterSellCommand.js
@@ -1,73 +1,80 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const fetch = require('cross-fetch');
-const dotenv = require('dotenv');
+const { SlashCommandBuilder } = require('@discordjs/builders')
+const fetch = require('cross-fetch')
+const dotenv = require('dotenv')
 dotenv.config()
 
-const {
-    replyFetchingDataEmbed,
-    replyNoSpacesInNameEmbed,
-    replyPlayerNameNotFoundOrInvalidEmbed,
-    profitAferSellEmbedReply
-} = require('../exports/embed.js')
-
+const { replyFetchingDataEmbed, replyNoSpacesInNameEmbed, replyPlayerNameNotFoundOrInvalidEmbed, profitAferSellEmbedReply } = require('../exports/embed.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('sellprofit')
         .setDescription('how much profit you will make after your ah sells')
-        .addStringOption(option =>
-            option.setName('name')
-                .setDescription('the username of the player')
-                .setRequired(true)
-        ),
+        .addStringOption(option => option.setName('name').setDescription('the username of the player').setRequired(true)),
     async execute(interaction, isEphemeral) {
         let name = interaction.options.getString('name')
 
-        if (name.split(" ").length > 1) {
-            return replyNoSpacesInNameEmbed(interaction, isEphemeral);
+        if (name.split(' ').length > 1) {
+            return replyNoSpacesInNameEmbed(interaction, isEphemeral)
         }
 
-        let res = await fetch(`${process.env.API_ENDPOINT}/search/player/${name}`);
-        let playerResponse = await res.json();
-        if (playerResponse.Slug === "player_not_found") {
-            return replyPlayerNameNotFoundOrInvalidEmbed(interaction, isEphemeral);
+        let res = await fetch(`${process.env.API_ENDPOINT}/search/player/${name}`)
+        let playerSearchResponse = await res.json()
+        if (playerSearchResponse.Slug === 'player_not_found') {
+            return replyPlayerNameNotFoundOrInvalidEmbed(interaction, isEphemeral)
         }
 
         await replyFetchingDataEmbed(interaction, isEphemeral)
         // Fetches the most recent auctions plus all active
-        let response = await fetch(`https://api.hypixel.net/skyblock/auction?key=${process.env.API_KEY}&player=${playerResponse[0].uuid}`);
-        let apiResponse = await response.json();
-        let uidList = []
-        let auctionPrice = []
-        let itemTag = []
-        // fetch /auction api for info about uid and bids
+        let response = await fetch(`https://api.hypixel.net/skyblock/auction?key=${process.env.API_KEY}&player=${playerSearchResponse[0].uuid}`)
+        let hypixelPlayerResponse = await response.json()
+
         try {
-            let promises = []
-            for (let i = 0; i < apiResponse.auctions.length; i++) {
-                let promise = fetch(`${process.env.API_ENDPOINT}/auction/${apiResponse.auctions[i].uuid}`)
-                promises.push(promise)
-            }
-            let processUidList = await Promise.all(promises);
-            promises = [];
-            //parses /auctions call to json
+            let auctionDetailsList = await fetchApiRequests(
+                hypixelPlayerResponse.auctions.map(auction => fetch(`${process.env.API_ENDPOINT}/auction/${auction.uuid}`))
+            )
+            auctionDetailsList = auctionDetailsList.filter(auction => new Date(auction.end) > new Date())
 
-            for (let i = 0; i < processUidList.length; i++) {
-                let promise = processUidList[i].json()
-                promises.push(promise)
-            }
-            // gets the starting bid from the /auctions call
-            processUidList = await Promise.all(promises);
-            processUidList = processUidList.filter(element => new Date(element.end) > new Date())
+            let soldAuctions = await fetchApiRequests(
+                auctionDetailsList.map(auction => fetch(`${process.env.API_ENDPOINT}/auctions/uid/${auction.flatNbt.uid}/sold`))
+            )
+            soldAuctions.forEach(auctions => {
+                auctions = auctions.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime())
+            })
 
-            for (let i = 0; i < processUidList.length; i++) {
-                // gets the uid from the /auction fetch
-                uidList.push(processUidList[i].flatNbt.uid)
-                // pushes the starting bid price
-                auctionPrice.push(processUidList[i].startingBid)
+            auctionDetailsList.forEach((auctionDetails, index) => {
+                auctionDetails.lastSoldAuctionUUID = soldAuctions[index].length > 0 ? soldAuctions[index][0].uuid : undefined
+            })
+            auctionDetailsList = auctionDetailsList.filter(auctionDetails => !!auctionDetails.lastSoldAuctionUUID)
+
+            let soldAuctionsDetails = await fetchApiRequests(
+                auctionDetailsList.map(auctionDetails => fetch(`${process.env.API_ENDPOINT}/auction/${auctionDetails.lastSoldAuctionUUID}`))
+            )
+
+            let totalProfit = 0
+            for (let i = 0; i < soldAuctionsDetails.length; i++) {
+                auctionDetailsList[i].purchasePrice = soldAuctionsDetails[i].startingBid
+                auctionDetailsList[i].itemName = soldAuctionsDetails[i].itemName
+                totalProfit += Number(auctionDetailsList[i].startingBid - soldAuctionsDetails[i].startingBid)
             }
-            return profitAferSellEmbedReply(interaction, isEphemeral, apiResponse, playerResponse, uidList, itemTag, auctionPrice)
+
+            let player = {
+                uuid: playerSearchResponse[0].uuid,
+                name: playerSearchResponse[0].name
+            }
+            return profitAferSellEmbedReply(interaction, isEphemeral, player, auctionDetailsList, totalProfit)
         } catch (error) {
             console.error(error)
         }
     }
+}
+
+async function fetchApiRequests(fetchPromises) {
+    let responses = await Promise.all(fetchPromises)
+    let promises = []
+    responses.forEach(res => {
+        if (res.status < 400 && res.status !== 204) {
+            promises.push(res.json())
+        }
+    })
+    return Promise.all(promises)
 }
--- exports/embed.js
+++ exports/embed.js
@@ -1,12 +1,9 @@
-const { MessageEmbed } = require('discord.js');
-const fetch = require('cross-fetch');
-const {
-    formatToPriceToShorten,
-    numberWithThousandsSeperators
-} = require('./formatFunctions')
+const { MessageEmbed } = require('discord.js')
+const fetch = require('cross-fetch')
+const { formatToPriceToShorten, numberWithThousandsSeperators } = require('./formatFunctions')
 
 // embed color
-const COLOR_EMBEDED_MESSAGES = ('#0099ff')
+const COLOR_EMBEDED_MESSAGES = '#0099ff'
 
 async function replyDaysOutOfBoundsEmbed(isEphemeral, interaction) {
     const embeded = new MessageEmbed()
@@ -45,20 +42,8 @@ async function replyFetchingDataEmbed(interaction, isEphemeral) {
 }
 
 async function nanErrorReplyEmbed(isEphemeral, interaction, playerData) {
-    const errorReply = new MessageEmbed()
-        .setColor(COLOR_EMBEDED_MESSAGES)
-        .setAuthor('Error!')
-        .setDescription(playerData)
-        .setTimestamp()
+    const errorReply = new MessageEmbed().setColor(COLOR_EMBEDED_MESSAGES).setAuthor('Error!').setDescription(playerData).setTimestamp()
     return await interaction.editReply({ embeds: [errorReply], ephemeral: isEphemeral })
-}
-
-function getProfitOrLossString(worstFlip) {
-    // checks if profit is positive or negitive
-    if (worstFlip.profit > 0) {
-        return `total profit being`
-    }
-    return `total loss being`
 }
 
 function getTotalProfitString(flipData, days) {
@@ -67,13 +52,6 @@ function getTotalProfitString(flipData, days) {
         return `**${formatToPriceToShorten(flipData.totalProfit, 0)}** in the last ${days} days`
     }
     return `**Nothing** in the last ${days} days`
-}
-
-function getProfitOrLossString(totalProfit) {
-    if (totalProfit > 0) {
-        return `will **make**`
-    }
-    return `will **lose**`
 }
 
 async function replyProfitEmbed(interaction, playerUUID, playerName, days, flipData, isEphemeral) {
@@ -93,8 +71,19 @@ async function replyProfitEmbed(interaction, playerUUID, playerName, days, flipD
         profitEmbed = profitEmbed.addField('Error!', `There where no flips found :frowning2:`)
     } else {
         // adds the highest profit and lowest profit flips to the embed
-        profitEmbed = profitEmbed.addField(`The highest profit flip was`, `${bestFlip.itemName} bought for ${formatToPriceToShorten(bestFlip.pricePaid)} sold for ${formatToPriceToShorten(bestFlip.soldFor)} profit being **${formatToPriceToShorten(bestFlip.profit)}**`)
-        profitEmbed = profitEmbed.addField(`**Worst Buy -> Sell**`, `${worstFlip.itemName} bought for ${formatToPriceToShorten(worstFlip.pricePaid)} sold for ${(formatToPriceToShorten(worstFlip.soldFor))} ${getProfitOrLossString(worstFlip)} **${formatToPriceToShorten(worstFlip.profit)}**`)
+        profitEmbed = profitEmbed.addField(
+            `The highest profit flip was`,
+            `${bestFlip.itemName} bought for ${formatToPriceToShorten(bestFlip.pricePaid)} sold for ${formatToPriceToShorten(
+                bestFlip.soldFor
+            )} profit being **${formatToPriceToShorten(bestFlip.profit)}**`
+        )
+
+        profitEmbed = profitEmbed.addField(
+            `**Worst Buy -> Sell**`,
+            `${worstFlip.itemName} bought for ${formatToPriceToShorten(worstFlip.pricePaid)} sold for ${formatToPriceToShorten(worstFlip.soldFor)} ${
+                worstFlip.profit >= 0 ? 'total profit being' : 'total loss being'
+            } **${formatToPriceToShorten(worstFlip.profit)}**`
+        )
     }
     return await interaction.editReply({ embeds: [profitEmbed], ephemeral: isEphemeral })
 }
@@ -111,7 +100,10 @@ async function replyLowestBinEmbed(interaction, isEphemeral, apiResponse, player
     // makes sure theres a lowest bin for the item
     if (playerResponse) {
         // adds the lowest bin of the item to the embed
-        lowestBinEmbed = lowestBinEmbed.addField(`The lowest bin of ${playerResponse[0].name}`, `is **${(numberWithThousandsSeperators(apiResponse.lowest))}** second lowest bin is ${(numberWithThousandsSeperators(apiResponse.secondLowest))}`)
+        lowestBinEmbed = lowestBinEmbed.addField(
+            `The lowest bin of ${playerResponse[0].name}`,
+            `is **${numberWithThousandsSeperators(apiResponse.lowest)}** second lowest bin is ${numberWithThousandsSeperators(apiResponse.secondLowest)}`
+        )
     } else {
         // or adds an error saying there was no lbin for the item
         playerResponse = lowestBinEmbed.addField('Error!', `There was no lbin for that item`)
@@ -130,7 +122,6 @@ async function itemInputWasNotFoundEmbedReply(interaction, isEphemeral) {
 }
 
 async function bidsReplyEmbed(interaction, isEphemeral, apiResponse) {
-    let bids = apiResponse
     let exampleEmbed = new MessageEmbed()
         .setColor(COLOR_EMBEDED_MESSAGES)
         .setURL('https://discord.js.org/')
@@ -139,83 +130,32 @@ async function bidsReplyEmbed(interaction, isEphemeral, apiResponse) {
         .setTimestamp()
     // goes through all of the api response to add the bids to the embed
     for (let i = 0; i < apiResponse.length; i++) {
-        exampleEmbed = exampleEmbed.addField(`${i + 1}.`, `**${apiResponse.itemName}** | Highest bid:  **${numberWithThousandsSeperators(apiResponse.highestBid)}** Coins | BIN: ${apiResponse.bin}`)
+        exampleEmbed = exampleEmbed.addField(
+            `${i + 1}.`,
+            `**${apiResponse.itemName}** | Highest bid:  **${numberWithThousandsSeperators(apiResponse.highestBid)}** Coins | BIN: ${apiResponse.bin}`
+        )
     }
     return await interaction.editReply({ embeds: [exampleEmbed], ephemeral: isEphemeral })
 }
 
-
-async function profitAferSellEmbedReply(interaction, isEphemeral, apiResponse, playerResponse, processUidList, itemTag, auctionPrice) {
-    let promises = [];
-    let auctionUuid = [];
-    for (let i = 0; i < processUidList.length; i++) {
-        let promise = fetch(`${process.env.API_ENDPOINT}/auctions/uid/${processUidList[i]}/sold`)
-        promises.push(promise)
+async function profitAferSellEmbedReply(interaction, isEphemeral, player, auctionDetailsList, totalProfit) {
+    let profitAfterSellEmbed = new MessageEmbed()
+        .setColor(COLOR_EMBEDED_MESSAGES)
+        .setAuthor('Profit after sell')
+        // command users id
+        .setDescription(`<@${interaction.member.user.id}>`)
+        .setThumbnail(`https://crafatar.com/renders/body/${player.uuid}`)
+        .addField(`${player.name} ${totalProfit >= 0 ? 'will **make**' : 'will **lose**'}`, `**${formatToPriceToShorten(totalProfit)}** from their auctions`)
+        .setTimestamp()
+    for (let i = 0; i < auctionDetailsList.length; i++) {
+        profitAferSellEmbed = profitAfterSellEmbed.addField(
+            `${auctionDetailsList[i].itemName} was bought for`,
+            `**${formatToPriceToShorten(auctionDetailsList[i].purchasePrice)}** and it will sell for **${formatToPriceToShorten(
+                auctionDetailsList[i].startingBid
+            )}**`
+        )
     }
-    try {
-        let soldAuctions = await Promise.all(promises);
-        promises = [];
-        for (let i = 0; i < soldAuctions.length; i++) {
-            let promise = soldAuctions[i].json()
-            promises.push(promise)
-        }
-        soldAuctions = await Promise.all(promises);
-
-        for (let i = 0; i < soldAuctions.length; i++) {
-            if (soldAuctions[i].length === 0) {
-                soldAuctions.splice(i, 1)
-                apiResponse.splice(i, 1)
-                processUidList.splice(i, 1)
-                auctionPrice.splice(i, 1)
-                continue
-            }
-            let e = soldAuctions[i]
-            auctionUuid.push(e[0].uuid)
-        }
-        promises = [];
-        for (let i = 0; i < auctionUuid.length; i++) {
-            let res = await fetch(`${process.env.API_ENDPOINT}/auction/${auctionUuid[i]}`)
-            promises.push(res)
-        }
-        let auctionList = await Promise.all(promises)
-        promises = [];
-        for (let i = 0; i < auctionList.length; i++) {
-            let res = auctionList[i].json()
-            promises.push(res);
-        }
-        auctionList = await Promise.all(promises)
-
-        let itemName = [];
-        let boughtPrice = [];
-        for (let i = 0; i < auctionList.length; i++) {
-            boughtPrice.push(auctionList[i].startingBid)
-            itemName.push(auctionList[i].itemName)
-        }
-        let auctionProfit = [];
-        for (let i = 0; i < boughtPrice.length; i++) {
-            auctionProfit.push(auctionPrice[i] - boughtPrice[i])
-        }
-        totalProfit = 0;
-        for (let i = 0; i < auctionProfit.length; i++) {
-            totalProfit += Number(auctionProfit[i])
-        }
-
-        let profitAfterSellEmbed = new MessageEmbed()
-            .setColor(COLOR_EMBEDED_MESSAGES)
-            .setAuthor('Profit after sell')
-            // command users id
-            .setDescription(`<@${interaction.member.user.id}>`)
-            .setThumbnail(`https://crafatar.com/renders/head/${playerResponse[0].uuid}`)
-            .addField(`${playerResponse[0].name} ${getProfitOrLossString(totalProfit)}`, `**${formatToPriceToShorten(totalProfit)}** from there actions`)
-            .setTimestamp()
-            for (let i = 0; i < boughtPrice.length; i++) {
-                profitAferSellEmbed = profitAfterSellEmbed.addField(`${itemName[i]} was bought for`, `**${formatToPriceToShorten(boughtPrice[i])}** and it will sell for **${formatToPriceToShorten(auctionPrice[i])}**`)
-            }
-        return await interaction.editReply({ embeds: [profitAfterSellEmbed], ephemeral: isEphemeral })
-    } catch (error) {
-        console.error(error)
-    }
-
+    return await interaction.editReply({ embeds: [profitAfterSellEmbed], ephemeral: isEphemeral })
 }
 
 module.exports = {
